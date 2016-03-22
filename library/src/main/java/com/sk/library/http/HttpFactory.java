77a4700b143340254f1a3ec1617cd7c31c38bed0
Javen <52113784@qq.com>
2016-03-22 15:12:01 +0800

    测试提交

--- library/src/main/java/com/sk/library/http/HttpFactory.java
+++ library/src/main/java/com/sk/library/http/HttpFactory.java
@@ -1,11 +1,8 @@
 package com.sk.library.http;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.util.Log;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by sk on 16/2/23.
@@ -25,15 +22,7 @@ public class HttpFactory {
             String clsName = pkgName + PREFIX + HttpContant.HTPPMODE + SUFFIX;
             Constructor mConstructor = Class.forName(clsName).getConstructor(Context.class, HttpListener.class);
             iHttpHelper = (IHttpHelper) mConstructor.newInstance(context, httpListener);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return iHttpHelper;
